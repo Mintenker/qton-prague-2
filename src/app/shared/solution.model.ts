93a84a19e5d8a176f1a7c4bc13c59fe727d54797
rysavyvladan <rysavy.vladan@gmail.com>
2017-11-26 11:14:34 +0100

    feat(project): added more projects

--- src/app/shared/solution.model.ts
+++ src/app/shared/solution.model.ts
@@ -1,18 +1,20 @@
-import { ResultModel } from './result.model';
-import { CommentModel } from './comment.model';
+import {ResultModel} from './result.model';
+import {CommentModel} from './comment.model';
 
 export interface SolutionModel {
-  id: number;
-  vendor: string;
-  year: number;
-  status: string;
-  category: string;
-  description: string;
-  longDescription: string;
-  title: string;
-  comments: CommentModel[];
-  budget: number;
-  location: string;
-  historicalResults?: ResultModel[];
-  image: string;
+    id: number;
+    vendor: string;
+    year: number;
+    status: string;
+    category: string;
+    description: string;
+    longDescription: string;
+    title: string;
+    comments: CommentModel[];
+    budget: number;
+    location: string;
+    historicalResults?: ResultModel[];
+    image: string;
+    referenceVendor: string;
+    referenceCity: string;
 }
