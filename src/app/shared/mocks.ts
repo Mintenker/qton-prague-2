f153515831cfdf5bf4e8afd42f8cb480bcaa4b8d
rysavyvladan <rysavy.vladan@gmail.com>
2017-11-26 10:24:22 +0100

    feat(project): detail of project

--- src/app/shared/mocks.ts
+++ src/app/shared/mocks.ts
@@ -1,40 +1,49 @@
-import { SolutionModel } from './solution.model';
+import {SolutionModel} from './solution.model';
 
 export const MOCK_SOLUTIONS: SolutionModel[] = [
-  {
-    id: 1,
-    vendor: 'Flowup',
-    title: 'Best project',
-    description: 'Best project eveeeeeer',
-    longDescription: 'Best project eveeeeeeer - even longer',
-    comments: [],
-    historicalResults: [],
-    budget: 200000,
-    location: 'Prague',
-    image: 'assets/images/smart-city-mock.jpg'
-  },
-  {
-    id: 2,
-    vendor: 'Smart Prague',
-    title: 'Smart lavicky',
-    description: 'Smart lavicky su celkovo nic moc :D',
-    longDescription: 'Smart lavicky su celkovo nic moc :D',
-    comments: [],
-    historicalResults: [],
-    budget: 500000,
-    location: 'Prague',
-    image: 'assets/images/smart-city-mock.jpg'
-  },
-  {
-    id: 3,
-    vendor: 'Dumb Prague',
-    title: 'Dumb lavicky',
-    description: 'Dumb lavicky su celkovo dost dobre :D',
-    longDescription: 'Dumb lavicky su celkovo dost dobre :D',
-    comments: [],
-    historicalResults: [],
-    budget: 500,
-    location: 'Prague',
-    image: 'assets/images/smart-city-mock.jpg'
-  },
+    {
+        id: 1,
+        year: 2017,
+        status: 'dokonceno',
+        category: 'categorie',
+        vendor: 'Flowup',
+        title: 'Best project',
+        description: 'Best project eveeeeeer',
+        longDescription: 'Best project eveeeeeeer - even longer',
+        comments: [],
+        historicalResults: [],
+        budget: 200000,
+        location: 'Prague',
+        image: 'assets/images/smart-city-mock.jpg'
+    },
+    {
+        id: 2,
+        year: 2017,
+        status: 'dokonceno',
+        category: 'categorie',
+        vendor: 'Smart Prague',
+        title: 'Smart lavicky',
+        description: 'Smart lavicky su celkovo nic moc :D',
+        longDescription: 'Smart lavicky su celkovo nic moc :D',
+        comments: [],
+        historicalResults: [],
+        budget: 500000,
+        location: 'Prague',
+        image: 'assets/images/smart-city-mock.jpg'
+    },
+    {
+        id: 3,
+        year: 2017,
+        status: 'dokonceno',
+        category: 'categorie',
+        vendor: 'Dumb Prague',
+        title: 'Dumb lavicky',
+        description: 'Dumb lavicky su celkovo dost dobre :D',
+        longDescription: 'Dumb lavicky su celkovo dost dobre :D',
+        comments: [],
+        historicalResults: [],
+        budget: 500,
+        location: 'Prague',
+        image: 'assets/images/smart-city-mock.jpg'
+    },
 ];
--- src/app/shared/solution.model.ts
+++ src/app/shared/solution.model.ts
@@ -4,6 +4,9 @@ import { CommentModel } from './comment.model';
 export interface SolutionModel {
   id: number;
   vendor: string;
+  year: number;
+  status: string;
+  category: string;
   description: string;
   longDescription: string;
   title: string;
